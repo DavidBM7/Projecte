78878bbd52b26d58c8e6aac6ad8ebf2bff45527d
DavidBM7 <148210975+DavidBM7@users.noreply.github.com>
2024-06-14 08:20:28 +0200

    Add files via upload

--- ex6.py
+++ ex6.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask  # Importamos la clase Flask desde el módulo flask
 
-app = Flask(__name__)
+app = Flask(__name__)  # Creamos una instancia de la clase Flask, pasándole el nombre del módulo actual
 
+# Definimos una ruta para la URL principal del sitio ("/")
 @app.route('/')
 def home():
-    return "Hola mundo!!! (alt + F4 para recibir 5 euros)"
+    return "Hola mundo!!! (alt + F4 para recibir 5 euros)"  # Esta función retorna un mensaje cuando alguien visita la URL principal
 
 def pex6():
-    print("Se esta ejecutando")
-    app.run()
+    print("Se esta ejecutando")  # Imprimimos un mensaje en la consola indicando que la aplicación está en ejecución
+    app.run()  # Ejecutamos la aplicación Flask
 
+# Si este archivo se está ejecutando directamente (no importado como módulo), ejecutamos la función pex6
 if __name__ == '__main__':
     pex6()
